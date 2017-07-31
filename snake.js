4bca607cd352562765a5f44bf62ec2e0f9056172
aambroa <aambroa@hiberus.com>
2017-07-31 23:08:44 +0200

    Add game files

--- snake.js
+++ snake.js
@@ -2,40 +2,31 @@
   const GRID_SIZE = 25;
   const AREA_LIMIT = GRID_SIZE - 1;
   const INITIAL_SEGMENTS = 5;
-  const DIRECTIONS = {
-    left: [-1, 0],
-    right: [1, 0],
-    up: [0, -1],
-    down: [0, 1],
-  };
+  const DIRECTIONS = { left: [-1, 0], right: [1, 0], up: [0, -1], down: [0, 1] };
   const INITIAL_HEAD = { x: 11, y: 12, dir: DIRECTIONS.left };
   const GAME_STATE = { INIT: 0, RUN: 1, FAIL: 2 };
   const food = { x: 5, y: 5 };
+
   let score = 0;
+  let gameState = GAME_STATE.INIT;
+  let timer;
+  let clientScr;
+  let ctx;
+  let ringSize;
 
   const Snake = function (initx, inity, initdir) {
     const self = this;
     self.dir = initdir;
-    self.head = {
-      x: initx,
-      y: inity,
-    };
+    self.head = { x: initx, y: inity };
     self.tail = self.head;
     self.createSegment = function (pX, pY, p, n) {
       return { x: pX, y: pY, prev: p, next: n };
     };
     self.addSegment = function () {
       const segment = self.createSegment(self.tail.x - self.dir[0], self.tail.y - self.dir[1], self.tail, null);
-      self.tail.next = segment;
-      self.tail = segment;
+      self.tail.next = self.tail = segment;
     };
   };
-
-  let gameState = GAME_STATE.INIT;
-  let timer;
-  let clientScr;
-  let ctx;
-  let ringSize;
 
   init();
 
@@ -48,7 +39,6 @@
     snake = createSnake(INITIAL_HEAD, INITIAL_SEGMENTS);
     renderInit();
   }
-
   function createSnake(headData, numSegments) {
     const snake = new Snake(headData.x, headData.y, headData.dir);
     for (let s = 0; s <= numSegments; s++) {
@@ -56,20 +46,17 @@
     }
     return snake;
   }
-
   function update() {
     let state;
     render();
     state = check();
     if (state === GAME_STATE.FAIL) stopGame(timer);
-    move();
+    else move();
   }
-
   function stopGame() {
     clearInterval(timer);
     printMsg('Game Over');
   }
-
   function renderInit() {
     printMsg('');
     clearScr();
@@ -80,7 +67,6 @@
     }
     drawHead();
   }
-
   function handleKeyUp(e) {
     switch (e.keyCode) { case 32: start(); }
   }
@@ -92,7 +78,6 @@
       case 39: snake.dir = DIRECTIONS.right;
     }
   }
-
   function start() {
     if (gameState !== GAME_STATE.INIT) {
       if (timer) clearInterval(timer);
@@ -104,7 +89,6 @@
     gameState = GAME_STATE.RUN;
     timer = setInterval(update, 50);
   }
-
   function check() {
     checkLimits();
     checkFood();
@@ -147,7 +131,6 @@
     drawSegment(snake.head.next.x, snake.head.next.y);
     drawFood();
   }
-
   function move() {
     eraseTail();
     snake.tail.x = snake.head.x + snake.dir[0];
@@ -160,17 +143,14 @@
     snake.tail = snake.tail.prev;
     snake.tail.next = null;
   }
-
   function clearScr() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, clientScr.width, clientScr.height);
-  };
-
+  }
   function drawSegment(x, y) {
     ctx.fillStyle = 'gray';
     ctx.fillRect(x * segmentSize, y * segmentSize, segmentSize, segmentSize);
   }
-
   function drawHead() {
     ctx.fillStyle = 'red';
     ctx.fillRect(snake.head.x * segmentSize, snake.head.y * segmentSize, segmentSize, segmentSize);
@@ -185,5 +165,9 @@
   }
   function printMsg(text) {
     doc.getElementById('msgs').innerHTML = text;
+  }
+  function drawElement(color, pX, pY, sX, sY) {
+    ctx.fillStyle = color;
+    ctx.fillRect(pX, snake.tail.y * segmentSize, segmentSize, segmentSize)
   }
 }(window, document));
