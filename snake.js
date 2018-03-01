46d25d8d0382ae2984b908cb79375201e9b7d811
alex <jandroz@gmail.com>
2018-03-01 02:41:12 +0100

    Improve code

--- snake.js
+++ snake.js
@@ -1,41 +1,51 @@
 (function (globalScope, doc) {
   const GRID_SIZE = 25;
   const AREA_LIMIT = GRID_SIZE - 1;
-  const INITIAL_SEGMENTS = 5;
+  const INITIAL_SEGMENTS = 3;
   const DIRECTIONS = { left: [-1, 0], right: [1, 0], up: [0, -1], down: [0, 1] };
   const INITIAL_HEAD = { x: 11, y: 12, dir: DIRECTIONS.left };
   const GAME_STATE = { INIT: 0, RUN: 1, FAIL: 2 };
   const AREA_BACKGROUND_COLOR = 'black';
-  const SPEED = 15; // steps per second
+  const SPEED = 1000 / 20; // steps per second
   const food = {};
 
   let score = 0;
   let gameState = GAME_STATE.INIT;
-  let timer;
   let clientScr;
   let ctx;
   let snake;
   let segmentSize;
+  let t1 = 0;
+  let frameRequest;
 
-  const Snake = function Snake(initx, inity, initdir) {
+  const Snake = function Snake(initx, inity, initdir, num_segments) {
     const self = this;
-    self.dir = initdir;
-    self.head = { x: initx, y: inity };
-    self.tail = self.head;
+
     self.createSegment = function createSegment(pX, pY, p, n) {
       return { x: pX, y: pY, prev: p, next: n };
     };
-    self.addSegment = function addSegment() {
-      const segment = self.createSegment(self.tail.x - self.dir[0], self.tail.y - self.dir[1], self.tail, null);
+    self.addSegment = function addSegment(cx, cy) {
+      const segment = self.createSegment(self.tail.x + cx, self.tail.y + cy, self.tail, null);
       self.tail.next = segment;
       self.tail = segment;
     };
+    self.draw = function() {
+      let segment = self.head.next;     
+      while (segment) {
+        drawElement('gray', segment.x, segment.y);
+        segment = segment.next;
+      }
+      drawElement('red', self.head.x , self.head.y);
+    }
+    self.dir = initdir;
+    self.head = self.createSegment(initx, inity, null, null);
+    self.tail = self.head;
+    for (let s = 0; s < num_segments; s++) {
+      self.addSegment(-initdir[0], -initdir[1]);
+    }
   };
   function createSnake(headData, numSegments) {
-    snake = new Snake(headData.x, headData.y, headData.dir);
-    for (let s = 0; s <= numSegments; s++) {
-      snake.addSegment();
-    }
+    snake = new Snake(headData.x, headData.y, headData.dir, INITIAL_SEGMENTS);
     return snake;
   }
   function placeFood() {
@@ -45,9 +55,7 @@
   function checkAutoCollision() {
     let segment = snake.head.next;
     while (segment) {
-      if (segment.x === snake.head.x && segment.y === snake.head.y) {
-        return true;
-      }
+      if (segment.x === snake.head.x && segment.y === snake.head.y) { return true; }
       segment = segment.next;
     }
     return false;
@@ -66,90 +74,76 @@
     if (food.x === snake.head.x && food.y === snake.head.y) {
       score++;
       showScore(score);
-      snake.addSegment();
+      snake.addSegment(0,0);
       placeFood();
     }
   }
   function drawElement(color, pX, pY, sX, sY) {
     ctx.fillStyle = color;
-    ctx.fillRect(pX, pY, sX, sY);
+    ctx.fillRect(pX * segmentSize, pY * segmentSize, segmentSize, segmentSize);
   }
   function clearScr() {
-    drawElement(AREA_BACKGROUND_COLOR, 0, 0, clientScr.width, clientScr.height);
-  }
-  function drawSegment(x, y) {
-    drawElement('gray', x * segmentSize, y * segmentSize, segmentSize, segmentSize);
-  }
-  function drawHead() {
-    drawElement('red', snake.head.x * segmentSize, snake.head.y * segmentSize, segmentSize, segmentSize);
+    ctx.fillStyle = AREA_BACKGROUND_COLOR;
+    ctx.fillRect(0, 0, clientScr.width, clientScr.height);
   }
   function drawFood() {
-    drawElement('green', food.x * segmentSize, food.y * segmentSize, segmentSize, segmentSize);
-  }
-  function eraseTail() {
-    drawElement(
-      AREA_BACKGROUND_COLOR, snake.tail.x * segmentSize, snake.tail.y * segmentSize, segmentSize, segmentSize);
+    drawElement('green', food.x , food.y);
   }
   function printMsg(text) {
     doc.getElementById('msgs').innerHTML = text;
   }
   function render() {
-    drawHead();
-    drawSegment(snake.head.next.x, snake.head.next.y);
+    clearScr();
     drawFood();
+    snake.draw();
   }
   function move() {
-    eraseTail();
     snake.tail.x = snake.head.x + snake.dir[0];
     snake.tail.y = snake.head.y + snake.dir[1];
-
-    snake.head.prev = snake.tail;
     snake.tail.next = snake.head;
+    snake.head.prev = snake.tail;
     snake.head = snake.tail;
-
     snake.tail = snake.tail.prev;
+    snake.head.prev = null;
     snake.tail.next = null;
   }
   function stopGame() {
-    clearInterval(timer);
+    cancelAnimationFrame(frameRequest);
     printMsg('Game Over');
   }
-  function renderInit() {
-    printMsg('');
-    clearScr();
-    let segment = snake.head.next;
-    while (segment) {
-      drawSegment(segment.x, segment.y);
-      segment = segment.next;
+  function update(t) {
+    if (checkAutoCollision()) {
+      gameState = GAME_STATE.FAIL;
+      stopGame();
+      render();
+      return;
     }
-    drawHead();
-  }
-  function update() {
-    move();
+    if (t - t1 > SPEED) {
+      move();
+      t1 = t;
+    }
     checkLimits();
     checkFood();
-    if (checkAutoCollision()) { gameState = GAME_STATE.FAIL; stopGame(timer); }
     render();
+    frameRequest = requestAnimationFrame(update);
   }
   function start() {
     if (gameState !== GAME_STATE.INIT) {
-      if (timer) clearInterval(timer);
       snake = createSnake(INITIAL_HEAD, INITIAL_SEGMENTS);
       score = 0;
-      showScore(0);
     }
+    printMsg('');
     placeFood();
-    renderInit();
     gameState = GAME_STATE.RUN;
-    timer = setInterval(update, 1000 / SPEED);
+    frameRequest = requestAnimationFrame(update);
   }
   function handleKeyUp(e) {
     switch (e.keyCode) {
       case 32: start(); break;
-      case 38: snake.dir = DIRECTIONS.up; break;
-      case 40: snake.dir = DIRECTIONS.down; break;
-      case 37: snake.dir = DIRECTIONS.left; break;
-      case 39: snake.dir = DIRECTIONS.right;
+      case 38: snake.dir = DIRECTIONS.up; move(); break;
+      case 40: snake.dir = DIRECTIONS.down; move(); break;
+      case 37: snake.dir = DIRECTIONS.left; move(); break;
+      case 39: snake.dir = DIRECTIONS.right; move();
     }
   }
   function init() {
@@ -158,8 +152,7 @@
     segmentSize = clientScr.width / GRID_SIZE;
     clientScr.addEventListener('keyup', handleKeyUp, true);
     snake = createSnake(INITIAL_HEAD, INITIAL_SEGMENTS);
-    renderInit();
+    render();
   }
-
   init();
 }(window, document));
